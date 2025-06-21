650eb3ffed0c7cc6b44af5414f246371e81cca8f
David Bergmann <david.bergmann@verndale.com>
2025-06-21 07:46:32 -0500

    Restore cookie bot

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
   server: {
     allowedHosts: ["intimate-katydid-equally.ngrok-free.app"],
   },
   optimizeDeps: {
     exclude: ["lucide-react"],
-  },
-  define: {
-    __ENABLE_USERCENTRICS__: JSON.stringify(mode === 'production'),
-  },
+  }
 }));
