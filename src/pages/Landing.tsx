6b5895728fa5f0a88c2a7911ce3d4d61ab8852ff
David Bergmann <david.bergmann@verndale.com>
2025-06-20 22:30:06 -0500

    Fix merge conflicts

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import { 
-  Sparkles, 
-  PenTool, 
-  Brain,
+import {
   Clock,
   TrendingUp,
   CheckCircle,
